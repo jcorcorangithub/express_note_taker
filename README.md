0a4faa871cf5283f3b30f7db4118aff2dc77669d
James Corcoran <jpcsoccer1234@gmail.com>
2021-05-01 21:08:08 -0400

    cleaned up code, added screenshots

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 The purpose if this assignment is to modify starter code to create an application that allow a user to write and save notes. 
 
 link to github repo:
-_______
+https://github.com/jcorcorangithub/express_note_taker
 
 
 link to video
@@ -24,6 +24,6 @@ I will be the sole contributor of this project
 Name: James Corcoran 
 email: jpcsoccer1234@gmail.com
 
-<!-- ![alt text](screenshots/screenshot.jpg)
+![alt text](screenshots/screenshot.jpg)
 ![alt text](screenshots/screenshot2.jpg)
-![alt text](screenshots/screenshot3.jpg) -->
+![alt text](screenshots/screenshot3.jpg)
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const notes = require('./db/db.json');
 
 const app = express();
 
@@ -19,26 +20,39 @@ app.get('/notes', (req, res) => {
 
 //read file from file system then send it back with res.json
 app.get('/api/notes', (req, res) => {
-    res.json('db/db.json');
+    res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-    let note = req.body;
-    note.id = uuidv4();
-    fs.writeFile('db/db.json', JSON.stringify(note), function (err) {
-        if (err) throw err;
+    req.body.id = uuidv4();
+    notes.push(req.body);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), function (err) {
+        if (err) throw err; 
     });
-    // res.sendFile(path.join(__dirname, 'db/db.json'));
+    res.json(notes);
 });
 
+app.delete('/api/notes/:id', (req,res) => {
+
+    for(i = 0; i < notes.length; i++){
+        if(req.params.id == notes[i].id){
+            notes.splice(i, 1);
+            break;
+        }
+    };
+    
+    fs.writeFile('db/db.json', JSON.stringify(notes), function (err) {
+        if (err) throw err; 
+    });
+
+    res.sendStatus(200);
+});
 
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-
-
-
 
 
 app.listen(PORT, () => {
